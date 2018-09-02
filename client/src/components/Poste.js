d20eca4c29dce5b93dae7ddf90d57f7dd6b771b8
kodakodu <kodakodu@gmail.com>
2018-09-02 20:41:15 +0200

    Ajout de la date

--- client/src/components/Poste.js
+++ client/src/components/Poste.js
@@ -4,7 +4,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import Moment from 'moment';
 
 const Poste = (props) => {
 return(
@@ -12,12 +12,15 @@ return(
         {props.poste ? (
                <Card style={{height:'250px',margin:'50px'}}>
                <CardContent style={{float:'left'}}>
-                   <Typography gutterBottom variant="headline" component="h2" style={{backgroundColor:'lightgray'}}>
+                   <Typography gutterBottom variant="display1" component="h2" style={{backgroundColor:'lightgray'}}>
                        {props.poste.titre}
+                   </Typography>
+                   <Typography variant="headline" gutterBottom>{Moment(props.poste.date).utc().format('DD-MM-YYYY')}
                    </Typography>
                    <Typography component="p" style={{width:'250px'}}>
                        {props.poste.description}
                    </Typography>
+                   
                </CardContent>
                <CardActions style={{float:'right'}}>
                    <Button variant="contained" size="medium" color="secondary" href={props.poste.date} target="_blank">
